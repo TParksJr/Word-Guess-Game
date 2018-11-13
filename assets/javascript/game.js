e0ca73d5171eecb2fc024e8dfa0da08c921e85cc
Tyson <tys_p@yahoo.ca>
2018-11-13 11:57:58 -0500

    added mobile responsive elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,26 +13,6 @@ $(function () {
         userGuess = "",
         alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    /*//setting initial stats
-    document.getElementById("wins").innerHTML = wins;
-    document.getElementById("losses").innerHTML = losses;
-    document.getElementById("remaining").innerHTML = remaining;
-    document.getElementById("guessed").innerHTML = guessed;
-    
-    //choosing a random word
-    word = options[Math.floor(Math.random() * options.length)];
-    console.log("the word is " + word);
-    
-    //getting an array of the letters that make up the word
-    var letters = Array.from(word);
-    console.log("letters are " + letters);
-    
-    //show the number of letters in the word and auto assign ids
-    for (i = 0; i < word.length; i++) {
-        //document.getElementById("word").innerHTML += `<span class="letters${i}">_, </span>`;
-        document.getElementById("word").innerHTML += "<span class=" + letters[i] + ">_ </span>";
-    };*/
-
     //reset function
     function reset() {
         remaining = 7;
@@ -43,9 +23,7 @@ $(function () {
         $("#remaining").text(remaining);;
         $("#guessed").text(guessed);
         word = options[Math.floor(Math.random() * options.length)];
-        console.log("the word is " + word);
         letters = Array.from(word);
-        console.log("letters are " + letters);
         document.getElementById("word").innerHTML = "";
         for (i = 0; i < word.length; i++) {
             $("#word").append("<span class='" + letters[i] + "'>_ </span>");
@@ -59,7 +37,6 @@ $(function () {
     //adding event listener
     $(document).on("keypress", function (event) {
         userGuess = event.key.toLowerCase();
-        console.log("userGuess is " + userGuess);
 
         if (alphabet.includes(userGuess)) {
             if (guessed.includes(userGuess)) {
@@ -73,8 +50,6 @@ $(function () {
                     correctGuesses.push(userGuess);
 
                     $("." + userGuess).html(userGuess + " ");
-                    console.log(correctGuesses);
-                    console.log(letters);
                 };
             }
             else {
